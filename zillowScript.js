a29edd2545ab26807f160c46539b61bfc6974fba
Jboltle <stacbolt2@gmail.com>
2024-02-15 20:26:12 -0600

    Added a checker for state in the string

--- zillowScript.js
+++ zillowScript.js
@@ -1,9 +1,70 @@
 const axios = require('axios');
 const fs = require('fs');
-
+const prompt = require('prompt-sync')({sigint: true});
 
 const main = async () => {
-    const url = `https://zillow56.p.rapidapi.com/search?location=${location}%2C%20tx`;
+    
+    const location = prompt('Enter State: ')
+    const states = [
+        "Alabama",
+        "Alaska",
+        "Arizona",
+        "Arkansas",
+        "California",
+        "Colorado",
+        "Connecticut",
+        "Delaware",
+        "Florida",
+        "Georgia",
+        "Hawaii",
+        "Idaho",
+        "Illinois",
+        "Indiana",
+        "Iowa",
+        "Kansas",
+        "Kentucky",
+        "Louisiana",
+        "Maine",
+        "Maryland",
+        "Massachusetts",
+        "Michigan",
+        "Minnesota",
+        "Mississippi",
+        "Missouri",
+        "Montana",
+        "Nebraska",
+        "Nevada",
+        "New Hampshire",
+        "New Jersey",
+        "New Mexico",
+        "New York",
+        "North Carolina",
+        "North Dakota",
+        "Ohio",
+        "Oklahoma",
+        "Oregon",
+        "Pennsylvania",
+        "Rhode Island",
+        "South Carolina",
+        "South Dakota",
+        "Tennessee",
+        "Texas",
+        "Utah",
+        "Vermont",
+        "Virginia",
+        "Washington",
+        "West Virginia",
+        "Wisconsin",
+        "Wyoming"
+    ];
+    for (i = 0; i< states.length(); i++){
+        for (j = 0; location != states.some(); j++){     
+    console.log(`Invalid state:  ${location, err}`)
+}
+}
+    
+    const url = 'https://zillow56.p.rapidapi.com/search?location=houston%2C%20tx';
+
     const options = {
         method: 'GET',
         headers: {
@@ -13,6 +74,7 @@ const main = async () => {
     };
     
     try {
+
         const response = await fetch(url, options);
         const result = await response.text();
         fs.writeFile('realEstateData.json', result, 'utf-8', (err) => {
