351fdf941ba76e666826550056fa9660f7a60c63
Jboltle <71600944+Jboltle@users.noreply.github.com>
2024-02-26 07:41:53 +0000

    Added the functionality needed

--- zillowScript.js
+++ zillowScript.js
@@ -13,45 +13,55 @@ const main = async () => {
     const state = prompt('Enter State: ');
     const city = prompt('Enter City: ');
 
-    function stateCheck(state, city) {
-        const stateArray = [
-            "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida",
-            "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
-            "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska",
-            "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
-            "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas",
-            "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
-        ];
-
-        if (!stateArray.includes(state)) {
-            console.error("Please enter a valid state");
-            return false;
-        } else {
-            console.log(`| ${state} ${city}`);
-            return true;
-        }
-    }
-
-    if (!stateCheck(state, city)) {
+    const isValidState = stateCheck(state, city);
+    if (!isValidState) {
         return;
     }
 
-    const bridgeAPI = `https://api.bridgedataoutput.com/api/v2/OData/reviews/Reviews?access_token=`;
     const serverToken = process.env.SERVER_TOKEN;
     const apiURL = accessTokenInsert(url, serverToken);
+
     ApiRequest(apiURL);
 };
 
+const stateCheck = (state, city) => {
+    const stateArray = [
+        "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida",
+        "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
+        "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska",
+        "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+        "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas",
+        "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+    ];
+
+    if (!stateArray.includes(state)) {
+        console.error("Please enter a valid state");
+        return false;
+    } else {
+        console.log(`| ${state} ${city}`);
+        return true;
+    }
+};
+
 const accessTokenInsert = (url, serverToken) => {
-    return url + serverToken;
+    const accessTokenPattern = /access_token=/;
+    const match = url.match(accessTokenPattern);
+    if (match) {
+        const index = match.index + match[0].length;
+        const apiURL = url.slice(0, index) + serverToken + url.slice(index);
+        console.log(apiURL);
+        return apiURL;
+    } else {
+        // If "access_token=" is not found, simply append the server token to the end of the URL
+        return url + "&access_token=" + serverToken;
+    }
 };
 
 const ApiRequest = (apiURL) => {
     axios.get(apiURL)
         .then(response => {
             const data = response.data;
-
-            fs.writeFileSync('realEstateData.json', JSON.stringify(data), 'utf-8');
+            fs.appendFileSync('realEstateData.json', JSON.stringify(data), 'utf-8');
             console.log("Data written to realEstateData.json");
         })
         .catch(error => {
